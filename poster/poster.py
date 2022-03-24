9bcb3a8f676ef888d87d2437adbf2c3f8537b0e4
LeonardoE95 <leonardotamiano95@gmail.com>
2022-03-24 03:43:42 +0100

    Added final poster

---
+++ poster/poster.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+   print("Laboratorio di Algoritmi")
+   print("========================")
+   
+   materie = ["Python", "Programmazione", "Algoritmi"]
+   print("Materie trattate: ")
+   print("\t" + ", ".join(materie))
+
+   print("Per ulteriori info: ") 
+   print("\t" + "https://tvstudents.xyz/news.txt")
